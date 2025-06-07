fae4d3d957bffe69871660b6df95eb1bc51bb309
oridwan <ogridwan@gmail.com>
2025-06-07 13:11:47 -0400

    update

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ set -e  # Exit immediately on error
 #    mkdir -p "$(dirname "$TARGET_LINK")"
 #    ln -s "$PWD/jsmol" "$TARGET_LINK"
 #fi
-
+ln -s $PWD/jsmol /opt/render/project/src/ase_root/ase/db/static/jsmol
 # 3. Start the ASE web server
 echo "Starting ASE web server..."
 ase db lego-sp2.db -w
